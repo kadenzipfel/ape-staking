5b7ca73f5ec0a0a69f8c9503a6d032f8c2cc76ac
kadenzipfel <kaden.zipfel@hotmail.com>
2022-12-16 09:50:06 -0800

    Add collection sizes to constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,8 @@
 pub const STAKING_CONTRACT: &str = "0x5954aB967Bc958940b7EB73ee84797Dc8a2AFbb9";
+
+// Main ID := 1
+pub const BAYC_LENGTH: usize = 10000;
+// Main ID := 2
+pub const MAYC_LENGTH: usize = 19431;
+// Main ID := 3
+pub const BAKC_LENGTH: usize = 9602;
