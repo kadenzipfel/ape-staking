fa4b3b119efc8b094251a9740f1916e11cf29c6b
kadenzipfel <kaden.zipfel@hotmail.com>
2022-12-16 11:10:43 -0800

    Retrieve position data for each nft in each collection and save to json file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use ethers::prelude::*;
 use ethers_flashbots::FlashbotsMiddleware;
 use gumdrop::Options;
 use reqwest::Url;
+use serde_json::{Map, Value};
 use std::{convert::TryFrom, sync::Arc, time::Duration};
 use tracing::{info, subscriber};
 use tracing_subscriber::FmtSubscriber;
@@ -71,9 +72,28 @@ async fn run<P: JsonRpcClient + 'static>(opts: Opts, provider: Provider<P>) -> a
 
     info!("Position: {:?}", position);
 
+    let mut positions = Map::new();
+
+    let mut collections: Vec<Value> = vec![];
     for i in 1..3 {
-        for j in 0..COLLECTION_SIZES[i] {
-            let position = staking
+        let mut collection = Map::new();
+
+        match i {
+            1 => collection.insert(String::from("name"), String::from("BAYC").into()),
+            2 => collection.insert(String::from("name"), String::from("MAYC").into()),
+            3 => collection.insert(String::from("name"), String::from("BAKC").into()),
+            _ => None,
+        };
+
+        let mut collection_positions: Vec<Value> = vec![];
+
+        for j in 0..COLLECTION_SIZES[i - 1] {
+            let mut position = Map::new();
+
+            position.insert(String::from("main_id"), i.into());
+            position.insert(String::from("nft_id"), j.into());
+
+            let result = staking
                 .nft_position(
                     U256::from_dec_str(&i.to_string()).unwrap(),
                     U256::from_dec_str(&j.to_string()).unwrap(),
@@ -82,9 +102,25 @@ async fn run<P: JsonRpcClient + 'static>(opts: Opts, provider: Provider<P>) -> a
                 .await
                 .unwrap();
 
+            position.insert(String::from("staked_amount"), result.0.to_string().into());
+            position.insert(String::from("rewards_debt"), result.1.to_string().into());
+
             info!("Position: {:?}", position);
+
+            collection_positions.push(position.into());
         }
+
+        collection.insert(String::from("positions"), collection_positions.into());
+        collections.push(collection.into());
     }
+
+    positions.insert(String::from("collections"), collections.into());
+
+    std::fs::write(
+        "positions.json",
+        serde_json::to_string_pretty(&positions).unwrap(),
+    )
+    .unwrap();
 
     Ok(())
 }
